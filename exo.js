0b26723c39d55751ae45c3509364d561ac104352
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-26 09:43:32 +0200

    add imperative function fibonacci

--- exo.js
+++ exo.js
@@ -8,7 +8,6 @@
 
 const air = (r) => 2 * Math.PI * r;
 
-
 const firstJanvierIsDimanche = () => {
   for (let year = 2014; year <= 2050; year++) {
     let d = new Date(year, 0, 1); // 1er JANVIER de chaque annee
@@ -25,7 +24,19 @@ const fibonacci = (i, array) => {
   return i == 0 ? array : fibonacci(i - 1, array); // si terminer on affiche le tableau sinon on continue
 };
 
-const stringToCode = (string) => { // fonction auxiliaire qui converti un string en un tableau de code ascii correspondant à chacunes des lettres
+const fibonacci_imp = (i) => {
+  let fibo = [0, 1];
+  for (let j = 0; j < i; j++) {
+    let n = fibo.length;
+    let k = fibo[n - 1];
+    let v = fibo[n - 2];
+    fibo.push(k + v);
+  }
+  return fibo;
+};
+
+const stringToCode = (string) => {
+  // fonction auxiliaire qui converti un string en un tableau de code ascii correspondant à chacunes des lettres
   let array = [];
   for (let i = 0; i < string.length; i++) {
     array.push(string.charCodeAt(i));
@@ -38,14 +49,15 @@ const decode_caesar = (message) => {
   let str = message.toLowerCase(); // on se concentre sur les codes ascii minuscules
   let words = str.split(" "); // Split la phrase en array de mot
 
-  for (let j = 1; j < 26; j++) { // Les 25 possibilités de décalement
+  for (let j = 1; j < 26; j++) {
+    // Les 25 possibilités de décalement
     let sentence = []; // Phrase finale
     words.forEach(function (word) {
       let charArr = stringToCode(word); // mot sous forme de tableau de lettre en code ascii
       let string_word = "";
       charArr.forEach((char) => {
         let k = char + j;
-        if (k > 122) k -= 123 - 97;  // si le decalement dépasse le code 122 (z minuscule) on revient au code ascii a (97)
+        if (k > 122) k -= 123 - 97; // si le decalement dépasse le code 122 (z minuscule) on revient au code ascii a (97)
         string_word += String.fromCharCode(k); // converti les code ascii avec les lettres correspondantes
       });
       sentence.push(string_word);
@@ -53,8 +65,6 @@ const decode_caesar = (message) => {
     result[j] = sentence; // Ajout au set
   }
   return result;
-}
-
-
+};
 
 export { air, firstJanvierIsDimanche, fibonacci, decode_caesar };
